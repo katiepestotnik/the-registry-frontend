8e50c8cb0045986d877fa6eccd9b971147dad7db
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-11-23 22:57:27 -0500

    temp hyperlink to page

---
+++ src/components/Hyperlink.js
@@ -0,0 +1,7 @@
+//Temp
+
+const Hyperlink = (props) => {
+    return <div><a href="http://localhost:3000/hol-registry">Holiday Registry</a> <a href="http://localhost:3000/wed-registry">Wedding Registry</a> </div>
+
+};
+export default Hyperlink;
