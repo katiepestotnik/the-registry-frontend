1d26cf7369c4503a848f62cc335c2e23214124e2
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-11-28 17:32:23 -0500

    register page change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = (props) => {
             <Link to="/signup" style={{textDecoration:"none"}}><h2 className="nav-decoration">REGISTER</h2></Link>
             </li>
         </ul>
-        <div className="home-content">
+        <div className="overlap-content">
             <h3>Welcome to your Registry</h3>
             <p className="home-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
