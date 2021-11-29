8e02502fa6c8f613751bea26f9c34cc19e52d367
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-11-28 22:25:54 -0500

    registry hyperlink css done

--- src/components/Hyperlink.js
+++ src/components/Hyperlink.js
@@ -5,10 +5,16 @@ const Hyperlink = (props) => {
     const [state, setState] = useContext(Context)
     console.log(state)
     return(
-    <div>
-            <Link to="/wed-registry"><h2>Wedding Registry</h2></Link>
-            <Link to="/hol-registry"><h2>Holiday Registry</h2></Link>
-    </div>
+        <div>
+        <h1 className="template-font">Choose between two registries...</h1>
+        <ul className="nav justify-content-center">
+            <li class="nav-item">
+        <Link to="/wed-registry" style={{textDecoration:"none"}}><div className="template-registry img-wedding"><p className="template-text">Wedding Registry</p></div></Link></li>
+        <li class="nav-item">
+        <Link to="/hol-registry" style={{textDecoration:"none"}}><div className="template-registry img-holiday"><p className="template-text">Holiday Registry</p></div></Link>
+        </li>
+        </ul>
+        </div>
     )
 };
 export default Hyperlink;
