cc2645f7b0c15860b222b13e95de531a022adf0e
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-11-30 02:17:02 -0500

    Refactored APi Display in Show Page

--- src/pages/HolidayShow.js
+++ src/pages/HolidayShow.js
@@ -33,13 +33,12 @@ function HolidayShow(props) {
   };
 
   const loaded = () => {
-    console.log(props.HolidayItem)
     return props.HolidayItem.map((item) => (
-      <div key={item._id}>
-          <ul>
-              <li>{item.itemName}</li>
-              <li>{item.itemDescription}</li>
-              <li>{item.itemUrl}</li>
+      <div key={item._id} className="card">
+          <ul className="list-group list-group-flush list-font">
+              <li className="list-group-item">{item.itemName}</li>
+              <li className="list-group-item">{item.itemDescription}</li>
+              <a href={item.itemUrl}><li className="list-group-item">Go to {item.itemName} URL</li></a>
             </ul>
     <Link to={`/hol-registry/${item._id}`}><button>EDIT</button></Link>
   <button id="delete" onClick={() => props.deleteHolidayItem(item._id)}>
@@ -58,65 +57,66 @@ const etsyCarousel = () => {
             <MDBCarouselItem key={products[0].id} className='active'>
             <MDBCarouselElement src={products[0].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[0].name}</h5>
+            <a href={products[0].url}><h5>{products[0].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[1].id}>
             <MDBCarouselElement src={products[1].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[1].name}</h5>
+            <a href={products[1].url}><h5>{products[1].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[2].id}>
             <MDBCarouselElement src={products[2].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[2].name}</h5>
+            <a href={products[2].url}><h5>{products[2].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[3].id}>
             <MDBCarouselElement src={products[3].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[3].name}</h5>
+            <a href={products[3].url}><h5>{products[3].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[4].id}>
             <MDBCarouselElement src={products[4].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[4].name}</h5>
+            <a href={products[4].url}><h5>{products[4].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[5].id}>
             <MDBCarouselElement src={products[5].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[5].name}</h5>
+            <a href={products[5].url}><h5>{products[5].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[6].id}>
             <MDBCarouselElement src={products[6].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[6].name}</h5>
+            <a href={products[6].url}><h5>{products[6].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[7].id}>
             <MDBCarouselElement src={products[7].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[7].name}</h5>
+            <a href={products[7].url}><h5>{products[7].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[8].id}>
             <MDBCarouselElement src={products[8].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[8].name}</h5>
+            <a href={products[8].url}><h5>{products[8].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
             <MDBCarouselItem key={products[9].id}>
             <MDBCarouselElement src={products[9].image} alt="Product" />
             <MDBCarouselCaption>
-              <h5>{products[9].name}</h5>
+            <a href={products[9].url}><h5>{products[9].name}</h5></a>
             </MDBCarouselCaption>
             </MDBCarouselItem>
         </MDBCarouselInner>
-        </MDBCarousel></>)
+        </MDBCarousel>
+        </>)
 }
 
   const loading = () => {
@@ -163,8 +163,8 @@ const etsyCarousel = () => {
         </div>
       </div>
       <div className="row">
-      <div className="col">{props.HolidayItem ? loaded() : loading()}</div>
-      <div className="col col-border">{props.ApiResponse? etsyCarousel() : loading()}</div>
+      <div className="col col-border col-left">{props.HolidayItem ? loaded() : loading()}</div>
+      <div className="col col-border col-right">{props.ApiResponse? etsyCarousel() : loading()}</div>
       </div>
     </section>
   );
